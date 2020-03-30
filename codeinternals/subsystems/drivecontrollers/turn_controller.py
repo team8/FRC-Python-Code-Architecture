42f5219f93976a35f22beefa04b0dfd333b33cc2
Aditya-Paly <aoberai05@gmail.com>
2020-03-30 01:01:13 -0700

    added some stuff for turn controller that finds the distance wheel has to move to turn x angle. Need to find target position unit and put that in code specifically.

--- codeinternals/subsystems/drivecontrollers/turn_controller.py
+++ codeinternals/subsystems/drivecontrollers/turn_controller.py
@@ -5,7 +5,7 @@ from codeinternals.utils.drive_outputs import DriveOutputs
 class TurnController(DriveControllerBase):
     __targetAngle = 0
 
-    def __init__(self, targetAngle): #TODO add some sort of python doc where it says - means left, + means right etc.
+    def __init__(self, targetAngle): # TODO add some sort of python doc where it says - means left, + means right etc.
         print("MoveController has started!")
         self.output = DriveOutputs()
         self.__targetAngle = targetAngle
