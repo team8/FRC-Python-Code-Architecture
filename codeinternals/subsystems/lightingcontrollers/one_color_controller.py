d5cfa5a857e42ba46b1f05ab35ab6b0c84f31c84
Alexis Kim <ajkim0630@gmail.com>
2020-03-29 18:32:20 -0700

    color file

--- codeinternals/subsystems/lightingcontrollers/one_color_controller.py
+++ codeinternals/subsystems/lightingcontrollers/one_color_controller.py
@@ -1,11 +1,17 @@
 from wpilib import AddressableLED
+from codeinternals.utils import color
 
 
 class OneColorController:
-    def __init__(self, color):
+    def __init__(self, color1):
         self.led = AddressableLED(1)  # todo find real port
 
+        a = color(color1)
+        self.h = a[0]
+        self.s = a[1]
+        self.v = a[2]
+
     def update(self):
-        self.data = self.led.LEDData(255, 0, 0)
+        self.data = self.led.LEDData(self.h, self.s, self.v)
 
         self.led.setData(self.data)
---
+++ codeinternals/utils/color.py
@@ -0,0 +1,28 @@
+def color(x):
+
+    if x == "red":
+        return 0, 255, 100
+
+    if x == "orange":
+        return 2, 247, 87
+
+    if x == "yellow":
+        return 30, 255, 20
+
+    if x == "green":
+        return 70, 255, 20
+
+    if x == "blue":
+        return 85, 247, 20
+
+    if x == "purple":
+        return 180, 247, 20
+
+    if x == "pink":
+        return 0, 200, 100
+
+    if x == "white":
+        return 0, 75, 20
+
+    if x == "off":
+        return 0, 0, 0
