d17f657cf97bd63594f21e74086ea5c3222818bb
Aditya-Paly <aoberai05@gmail.com>
2020-04-01 18:06:53 -0700

    added port constants class and corresponding stuff in hardware adapter

--- codeinternals/hardware_adapter.py
+++ codeinternals/hardware_adapter.py
@@ -2,26 +2,27 @@ from wpilib._wpilib import Joystick
 
 from codeinternals import ctre
 from codeinternals.ctre import PigeonIMU
+from codeinternals.constants import port_constants
 
 
 class HardwareAdapter:
     class DriveHardware:
         def __init__(self):
-            self.left_master_falcon = ctre.TalonFX(0)
-            self.left_slave_falcon = ctre.TalonFX(2)
-            self.right_master_falcon = ctre.TalonFX(3)
-            self.right_slave_falcon = ctre.TalonFX(4)
-            self.left_falcon_encoder = ctre.CANCoder(0)
-            self.right_falcon_encoder = ctre.CANCoder(3)
-            self.gyro = PigeonIMU(0)  # TODO put actual id
+            self.left_master_falcon = ctre.TalonFX(port_constants.left_master_falcon_id)
+            self.left_slave_falcon = ctre.TalonFX(port_constants.left_slave_falcon_id)
+            self.right_master_falcon = ctre.TalonFX(port_constants.right_master_falcon_id)
+            self.right_slave_falcon = ctre.TalonFX(port_constants.right_slave_falcon_id)
+            self.left_falcon_encoder = ctre.CANCoder(port_constants.left_falcon_encoder_id)
+            self.right_falcon_encoder = ctre.CANCoder(port_constants.right_falcon_encoder_id)
+            self.gyro = PigeonIMU(port_constants.gyro_id)
 
         def __getInstance__(self):
             return self
 
     class JoystickHardware:
         def __init__(self):
-            self.driveJoystick = Joystick(0)
-            self.turnJoystick = Joystick(1)
+            self.driveJoystick = Joystick(port_constants.drive_joystick_id)
+            self.turnJoystick = Joystick(port_constants.turn_joystick_id)
 
         def __getInstance__(self):
             return self
