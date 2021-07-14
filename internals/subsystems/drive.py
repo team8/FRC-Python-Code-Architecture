7eaed99f29c8ef9c4bb8691120a0ccbc36c11364
Luca Manolache <luca1230@gmail.com>
2021-07-14 16:58:44 -0700

    Added the OSR hardware

--- internals/subsystems/drive.py
+++ internals/subsystems/drive.py
@@ -1,5 +1,12 @@
 from enum import Enum
 
+from ctre import TalonFX, PigeonIMU
+from rev import CANSparkMax, MotorType
+from wpilib.controller import PIDController
+
+import internals.commands
+from robot import RobotName
+
 
 class DriveState(Enum):
     IDLE = 0
@@ -9,10 +16,32 @@ class DriveState(Enum):
 
 
 class Drive:
-    def __init__(self):
+    def __init__(self, robot_type):
+        if robot_type == RobotName.OSR:
+            self.left_master = CANSparkMax(1, MotorType.kBrushless)
+            left_spark_slave1 = CANSparkMax(2, MotorType.kBrushless)
+            left_spark_slave2 = CANSparkMax(3, MotorType.kBrushless)
+            self.right_master = CANSparkMax(12, MotorType.kBrushless)
+            right_spark_slave_1 = CANSparkMax(13, MotorType.kBrushless)
+            right_spark_slave_2 = CANSparkMax(15, MotorType.kBrushless)
+
+            left_spark_slave1.follow(self.left_master)
+            left_spark_slave2.follow(self.left_master)
+            right_spark_slave_1.follow(self.right_master)
+            right_spark_slave_2.follow(self.right_master)
+
+            # self.gyro = PigeonIMU()
+
         self.state = DriveState.IDLE
         self.wanted_state = None
         self.controller = None
+
+        self.maxWheelPercentOutput = 0.4
+        self.closedLoopRampSec = 2
+        self.positionConversionFactor = 1000
+        self.velocityConversionFactor = 1000
+        self.moveStraightGains = PIDController(0.0, 0.0, 0.0)
+        self.turnGains = PIDController(0.0, 0.0, 0.0)
 
     def update_state(self):
         pass
