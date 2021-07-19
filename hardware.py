55f10d63c8e000bdea4ed91b83f80dd6f93dd01a
Vedanth P <vedpadu@gmail.com>
2021-07-19 16:02:50 -0700

    bang banging controller - not tested

--- hardware.py
+++ hardware.py
@@ -1,8 +1,17 @@
 from ctre import TalonFX, PigeonIMU
+from wpilib._wpilib import Joystick
+import rev
 
-left_master = TalonFX(12)
-left_slave = TalonFX(13)
-right_master = TalonFX(2)
-right_slave = TalonFX(3)
+left_master = rev.CANSparkMax(12, rev.MotorType.kBrushless)
+left_slave = rev.CANSparkMax(13, rev.MotorType.kBrushless)
+left_slave_2 = rev.CANSparkMax(15, rev.MotorType.kBrushless)
+right_master = rev.CANSparkMax(1, rev.MotorType.kBrushless)
+right_slave = rev.CANSparkMax(2, rev.MotorType.kBrushless)
+right_slave_2 = rev.CANSparkMax(3, rev.MotorType.kBrushless)
 
+
+drive_joystick = Joystick(0)
+turn_joystick = Joystick(1)
+
+TalonFX(int)
 gyro = PigeonIMU(9)
--- robot.py
+++ robot.py
@@ -2,7 +2,7 @@ import wpilib
 
 from hardware import *
 from drive_time import *
-
+from bang_bang_turn import *
 
 # The file is already written, nothing needs to be done here. Write your code in user_robot_code.py!
 # noinspection PyPep8Naming
@@ -10,31 +10,31 @@ class Robot(wpilib.TimedRobot):
 
     def robotInit(self):
         left_slave.follow(left_master)
+        left_slave_2.follow(left_master)
         right_slave.follow(right_master)
+        right_slave_2.follow(right_master)
+        gyro.calibrate()
 
     def autonomousInit(self):
-        pass
-
-    def autonomousPeriodic(self):
-        pass
-
-    def teleopInit(self):
-        self.routines = {DriveForward(3000)}
+        self.routines = {BangBangTurn(90)}
         self.current_routine = None
 
-    def teleopPeriodic(self):
-        print(self.current_routine)
+    def autonomousPeriodic(self):
         if self.current_routine is None:
             if len(self.routines) != 0:
                 self.current_routine = self.routines.pop()
 
-        print(self.current_routine)
         self.current_routine.update()
 
-        print(self.current_routine)
         if self.current_routine.is_done():
             self.current_routine = None
-        print(self.current_routine)
+
+    def teleopInit(self):
+        pass
+
+    def teleopPeriodic(self):
+        left_master.set(0.75*(drive_joystick.getY()/2 - turn_joystick.getX()/2))
+        right_master.set(-0.75*(drive_joystick.getY()/2 + turn_joystick.getX()/2))
 
 
 if __name__ == "__main__":
