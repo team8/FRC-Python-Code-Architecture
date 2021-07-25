e26349196c1d7574731f90fe8b2c3f2d2095678d
Maxwell938 <71574636+Maxwell938@users.noreply.github.com>
2021-07-25 14:38:40 -0700

    Made the new-subsystems branch into a template for the campers

--- hardware.py
+++ hardware.py
@@ -2,16 +2,10 @@ from ctre import TalonFX, PigeonIMU
 from wpilib._wpilib import Joystick
 import rev
 
-left_master = rev.CANSparkMax(12, rev.MotorType.kBrushless)
-left_slave = rev.CANSparkMax(13, rev.MotorType.kBrushless)
-left_slave_2 = rev.CANSparkMax(15, rev.MotorType.kBrushless)
-right_master = rev.CANSparkMax(1, rev.MotorType.kBrushless)
-right_slave = rev.CANSparkMax(2, rev.MotorType.kBrushless)
-right_slave_2 = rev.CANSparkMax(3, rev.MotorType.kBrushless)
+#Init motors and joysticks here.
 
+#CANSparkMax
 
-drive_joystick = Joystick(0)
-turn_joystick = Joystick(1)
+#Joystick
 
-TalonFX(int)
 gyro = PigeonIMU(9)
--- robot.py
+++ robot.py
@@ -6,36 +6,28 @@ from bang_bang_turn import *
 
 # The file is already written, nothing needs to be done here. Write your code in user_robot_code.py!
 # noinspection PyPep8Naming
+
+#Write your robot code in the following methods
+
+#Init functions run when it is first executed
+#Periodic functions run every 20ms when still enabled
+
 class Robot(wpilib.TimedRobot):
 
     def robotInit(self):
-        left_slave.follow(left_master)
-        left_slave_2.follow(left_master)
-        right_slave.follow(right_master)
-        right_slave_2.follow(right_master)
-        gyro.calibrate()
+        pass
 
     def autonomousInit(self):
-        self.routines = {BangBangTurn(90)}
-        self.current_routine = None
+        pass
 
     def autonomousPeriodic(self):
-        if self.current_routine is None:
-            if len(self.routines) != 0:
-                self.current_routine = self.routines.pop()
-
-        self.current_routine.update()
-
-        if self.current_routine.is_done():
-            self.current_routine = None
+        pass
 
     def teleopInit(self):
         pass
 
     def teleopPeriodic(self):
-        left_master.set(0.75*(drive_joystick.getY()/2 - turn_joystick.getX()/2))
-        right_master.set(-0.75*(drive_joystick.getY()/2 + turn_joystick.getX()/2))
-
+        pass
 
 if __name__ == "__main__":
     wpilib.run(Robot)
